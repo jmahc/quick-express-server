cd5adc7ed3812a6db77236ee4eebcc1094f7ea74
Jordan McArdle <jordanmcardle@gmail.com>
2017-02-06 21:36:15 -0600

    it wasnt pushed?

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,9 +1,15 @@
 var constants = require('../constants');
 var utils = require('../utils');
+var offices = require('../data/dummy-data.json');
 
 module.exports = function(app) {
   app.get('/', function(request, response) {
     response = utils.setResponseHeaderCors(response, constants.CLIENT_URL);
     return response.sendFile(constants.SOURCE_ROOT + '/index.html');
+  });
+
+  app.get('/api/officestatus', function(request, response) {
+    response = utils.setResponseHeaderCors(response, constants.CLIENT_URL);
+    return response.status(200).json(offices);
   });
 };
