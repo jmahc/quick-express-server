79e7183ca9eb5c46bfac3621b77b85b2872f5121
Jordan McArdle <jordanmcardle@gmail.com>
2017-05-12 09:17:51 -0500

    moving laptops

--- src/routes/consignee.js
+++ src/routes/consignee.js
@@ -1,7 +1,6 @@
 var _ = require('lodash');
 var constants = require('../constants');
 var utils = require('../utils');
-var shippers = require('../data/shippers.json');
 var consignees = require('../data/consignees.json');
 
 function doesStringContainQuery(item, query) {
@@ -10,37 +9,12 @@ function doesStringContainQuery(item, query) {
 
 module.exports = function(app) {
   app.get('/api/consignees', function(request, response) {
-    response = utils.setResponseHeaderCors(response, constants.CLIENT_URL);
-    console.log('we have consignees endpoint!');
-    var consigneesObject = _(consignees).map(function(value) {
-      return {
-        name: value.name,
-        id: value.id,
-        address: {
-          address1: value.address.address1,
-          address2: value.address.address2,
-          city: value.address.city,
-          stateprov: value.address.stateprov,
-          postal: value.address.postal,
-          country: value.address.country
-        }
-      };
-    })
-    .orderBy('name')
-    .take(10);
-    return response.status(200).json(consigneesObject);
-  });
-
-  app.get('/api/consignees/:id', function(request, response) {
-    var searchQuery = request.params.id;
-    response = utils.setResponseHeaderCors(response, constants.CLIENT_URL);
-    if (request.params.id === undefined || request.params.id === null) {
-      return response.status(500);
+    var takeCount = 10;
+    if (request.query.page) {
+      takeCount = parseInt(request.query.page);
     }
-    var responseObject = _(consignees)
-      .filter(function(item) {
-        return doesStringContainQuery(item.name, searchQuery);
-      })
+    response = utils.setResponseHeaderCors(response, constants.CLIENT_URL);
+    var consigneesObject = _(consignees)
       .map(function(value) {
         const address = value.address;
         return {
@@ -56,7 +30,50 @@ module.exports = function(app) {
           }
         };
       })
-      .orderBy('name');
+      .orderBy('name')
+      .take(takeCount);
+    return response.status(200).json(consigneesObject);
+  });
+
+  app.get('/api/consignees/:id', function(request, response) {
+    var searchQuery = request.params.id || null;
+    var takeCount = 10;
+    if (request.query.page) {
+      takeCount = parseInt(request.query.page);
+    }
+    response = utils.setResponseHeaderCors(response, constants.CLIENT_URL);
+    if (request.params.id === undefined || request.params.id === null) {
+      return response.status(500);
+    }
+    var responseObject = _(consignees)
+      .filter(function(item) {
+        if (
+          searchQuery !== null &&
+          searchQuery !== undefined &&
+          item !== null &&
+          item !== undefined
+        ) {
+          return doesStringContainQuery(item.name, searchQuery);
+        }
+        return;
+      })
+      .map(function(value) {
+        const address = value.address;
+        return {
+          id: value.id,
+          name: value.name,
+          address: {
+            address1: address.address1,
+            address2: address.address2,
+            city: address.city,
+            stateprov: address.stateprov,
+            postal: address.postal,
+            country: address.country
+          }
+        };
+      })
+      .orderBy('name')
+      .take(takeCount);
     return response.status(200).json(responseObject);
   });
-}
+};
--- src/routes/shipper.js
+++ src/routes/shipper.js
@@ -3,48 +3,53 @@ var constants = require('../constants');
 var utils = require('../utils');
 var shippers = require('../data/shippers.json');
 
+function doesStringContainQuery(item, query) {
+  return _.startsWith(item.toLowerCase(), query.toLowerCase());
+}
+
 module.exports = function(app) {
   // This gets all the shippers - TODO - limit it to a certain number
   // based on the request-length (a vs. aa vs. aaa vs. AAA Autoparts, etc..)
-  app.get('/api/shipper', function(request, response) {
-    response = utils.setResponseHeaderCors(response, constants.CLIENT_URL);
-    console.log('we have shippers: shippers');
-    var shippersObject = shippers.map(function(val) {
-      return {
-        name: val.name,
-        consignee: val.consignee,
-        address: {
-          address1: val.address.address1,
-          address2: val.address.address2,
-          city: val.address.city,
-          stateprov: val.address.stateprov,
-          postal: val.address.postal,
-          country: val.address.country
-        }
-      };
-    });
-    return response.status(200).json(_.orderBy(shippersObject, 'name'));
-  });
-  app.get('/api/shipper/:id/:name', function(request, response) {
-    response = utils.setResponseHeaderCors(response, constants.CLIENT_URL);
-    var shipperIdentity = request.params.id;
-    var shipperName = request.params.name;
-    console.log('we will get the shippers consignee and ', shipperIdentity);
-    console.log('we will get the shippers consignee and ', shipperName);
-    if (request.params.id === undefined || request.params.id === null) {
-      return response.status(500);
+
+  // We have to assume that there is going to be an ID attached to the request...
+  app.get('/api/shippers/:id/:name/:shipperName', function(request, response) {
+    console.log('we have shippers endpoint! ==> /api/shippers/:id/:name/:shipperName');
+    console.info(request.params);
+    var searchQueryId = request.params.id || null;
+    var searchQueryName = request.params.name || null;
+    var searchQueryShipperName = request.params.shipperName || null;
+    var takeCount = 10;
+    if (request.query.page) {
+      takeCount = parseInt(request.query.page);
     }
-    var shippersObject = shippers
-      .filter(function(itemIdentity) {
-        return itemIdentity.id !== shipperIdentity;
+    response = utils.setResponseHeaderCors(response, constants.CLIENT_URL);
+    var shippersObject = _(shippers)
+      .filter(function(item) {
+        if (item !== null && item !== undefined) {
+          return item.consignee !== searchQueryId;
+          // return item.id !== searchQueryId;
+          // return doesStringContainQuery(item.id, searchQueryId);
+        }
+        return console.log('item is null for item.id');
       })
-      .filter(function(itemIdentity) {
-        return itemIdentity.name !== shipperName;
+      .filter(function(item) {
+        if (item !== null && item !== undefined) {
+          return item.name !== searchQueryName;
+          // return doesStringContainQuery(item.name, searchQueryName);
+        }
+        return console.log('item is null for item.name');
+      })
+      .filter(function(item) {
+        if (item !== null && item !== undefined) {
+          return item.name !== searchQueryShipperName;
+        }
+        return console.log('item is null for item.name & shipper name');
       })
       .map(function(value) {
-        var address = value.address;
+        const address = value.address;
         return {
           name: value.name,
+          consignee: value.consignee,
           id: value.id,
           address: {
             address1: address.address1,
@@ -55,7 +60,65 @@ module.exports = function(app) {
             country: address.country
           }
         };
-      });
-    return response.status(200).json(_.orderBy(shippersObject, 'name'));
+      })
+      .orderBy('name')
+      .take(10);
+    return response.status(200).json(shippersObject);
+  });
+  app.get('/api/shippers/:id/:name', function(request, response) {
+    console.log('we have shippers endpoint! ==> /api/shippers/:id/:name');
+    var searchQueryId = request.params.id || null;
+    var searchQueryName = request.params.name || null;
+    var takeCount = 10;
+    if (request.query.page) {
+      takeCount = parseInt(request.query.page);
+    }
+    response = utils.setResponseHeaderCors(response, constants.CLIENT_URL);
+    if (searchQueryId === undefined || searchQueryId === null) {
+      console.log('Shippers endpoint did not have a parameter _id_');
+      return response.status(500);
+    }
+    if (searchQueryName === undefined || searchQueryName === null) {
+      console.log('Shippers endpoint did not have a parameter _name_');
+      return response.status(500);
+    }
+    console.log('we will get the shippers consignee and id:', searchQueryId);
+    console.log('we will get the shippers consignee and name:', searchQueryName);
+    console.log('Query is:', takeCount);
+
+    var responseObject = _(shippers)
+      .filter(function(item) {
+        if (item !== null && item !== undefined) {
+          return item.consignee !== searchQueryId;
+          // return item.id !== searchQueryId;
+          // return doesStringContainQuery(item.id, searchQueryId);
+        }
+        return console.log('item is null for item.id');
+      })
+      .filter(function(item) {
+        if (item !== null && item !== undefined) {
+          return item.name !== searchQueryName;
+          // return doesStringContainQuery(item.name, searchQueryName);
+        }
+        return console.log('item is null for item.name');
+      })
+      .map(function(value) {
+        var address = value.address;
+        return {
+          id: value.id,
+          name: value.name,
+          address: {
+            address1: address.address1,
+            address2: address.address2,
+            city: address.city,
+            stateprov: address.stateprov,
+            postal: address.postal,
+            country: address.country
+          }
+        };
+      })
+      .orderBy('name')
+      .take(takeCount);
+    return response.status(200).json(responseObject);
   });
 };
